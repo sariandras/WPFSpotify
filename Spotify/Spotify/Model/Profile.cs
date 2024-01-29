ce3cf3f97df8d8b6a5bdc6561ffad4dc7ac5b51c
User <User@LENOVO-T480S>
2024-01-29 12:23:39 +0100

    added TopItems

--- Spotify/Spotify/Model/Profile.cs
+++ Spotify/Spotify/Model/Profile.cs
@@ -6,22 +6,6 @@ using System.Threading.Tasks;
 
 namespace Spotify.Model
 {
-    public class ExternalUrls
-    {
-        public string? Spotify { get; set; }
-    }
-    public class Followers
-    {
-        public string? Href { get; set; }
-        public int Total { get; set; }
-    }
-    public class ProfileImage
-    {
-        public string? Url { get; set; }
-        public int Height { get; set; }
-        public int Width { get; set; }
-    }
-
     public class Profile
     {
         public string? DisplayName { get; set; }
@@ -29,7 +13,7 @@ namespace Spotify.Model
         public ExternalUrls? ExternalUrls { get; set; }
         public string? Href { get; set; }
         public string? Id { get; set; }
-        public List<ProfileImage>? Images { get; set; }
+        public List<Image>? Images { get; set; }
         public string? Type { get; set; }
         public string? Uri { get; set; }
     }
---
+++ Spotify/Spotify/Model/TopItems.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotify.Model
+{
+    public class Item
+    {
+        public ExternalUrls? ExternalUrls { get; set; }
+        public Followers? Followers { get; set; }
+        public List<string>? Genres { get; set; }
+        public string? Href { get; set; }
+        public string? Id { get; set; }
+        public List<Image>? Images { get; set; }
+        public string? Name { get; set; }
+        public int Popularity { get; set; }
+        public string? Type { get; set; }
+        public string? Uri { get; set; }
+    }
+
+    public class TopItems
+    {
+        public string? Href { get; set; }
+        public int Limit { get; set; }
+        public string? Next { get; set; }
+        public int Offset { get; set; }
+        public string? Previous { get; set; }
+        public int Total { get; set; }
+        public List<Item>? Items { get; set; }
+    }
+}
