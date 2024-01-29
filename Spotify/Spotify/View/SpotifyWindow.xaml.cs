5506b0a5f37fcc78d3ddda95fbf0e5673d989855
Sári András <sari.andras@students.jedlik.eu>
2024-01-29 12:28:33 +0100

    Nagyon gyenge view

--- Spotify/Spotify/View/SpotifyWindow.xaml.cs
+++ Spotify/Spotify/View/SpotifyWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Spotify
+namespace Spotify.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SpotifyWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SpotifyWindow : Window
     {
-        public MainWindow()
+        public SpotifyWindow()
         {
             InitializeComponent();
         }
